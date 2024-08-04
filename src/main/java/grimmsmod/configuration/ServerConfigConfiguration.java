9de2049a49fb5a5d40f0f428ff92fa7dd22a42af
tyuXX <tyuxx@gsdistance.org>
2024-08-05 00:03:51 +0300

    Improve bleeding, TODO: add shouts

--- src/main/java/grimmsmod/configuration/ServerConfigConfiguration.java
+++ src/main/java/grimmsmod/configuration/ServerConfigConfiguration.java
@@ -9,14 +9,22 @@ public class ServerConfigConfiguration {
 	public static final ModConfigSpec.ConfigValue<Boolean> INSTWAND;
 	public static final ModConfigSpec.ConfigValue<Double> LVLMULT;
 	public static final ModConfigSpec.ConfigValue<Double> LVLEXPO;
+	public static final ModConfigSpec.ConfigValue<Boolean> SPRESTIGE;
+	public static final ModConfigSpec.ConfigValue<Boolean> SLEVEL;
+	public static final ModConfigSpec.ConfigValue<Double> SNLEVEL;
 	static {
-		BUILDER.push("performance");
+		BUILDER.push("Performance");
 		INSTLEVELUP = BUILDER.comment("Use a while loop so no leftover xp is left").define("Instant Level Up", true);
 		INSTWAND = BUILDER.comment("Wands don't run on ticks but are instant").define("Instant wands", false);
 		BUILDER.pop();
-		BUILDER.push("itemleveling");
+		BUILDER.push("Item Leveling");
 		LVLMULT = BUILDER.comment("Item level xp requirement multiplier.").define("Item LvL Mult", (double) 100);
 		LVLEXPO = BUILDER.comment("Item level xp requirement multiplier.").define("Item LvL Expo", (double) 1.1);
+		BUILDER.pop();
+		BUILDER.push("Shoutouts");
+		SPRESTIGE = BUILDER.comment("Sends chat message to all players when someone prestiges.").define("Shout Prestige", true);
+		SLEVEL = BUILDER.comment("Sends chat message to all players when someone levels up.").define("Shout Level", true);
+		SNLEVEL = BUILDER.comment("Shouts every <value> levels").define("Level for shout", (double) 10);
 		BUILDER.pop();
 
 		SPEC = BUILDER.build();
--- src/main/java/grimmsmod/procedures/OnEntityAttackedProcedure.java
+++ src/main/java/grimmsmod/procedures/OnEntityAttackedProcedure.java
@@ -11,6 +11,10 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.tags.TagKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.Registries;
 
 import javax.annotation.Nullable;
 
@@ -22,16 +26,16 @@ public class OnEntityAttackedProcedure {
 	@SubscribeEvent
 	public static void onEntityAttacked(LivingAttackEvent event) {
 		if (event.getEntity() != null) {
-			execute(event, event.getEntity(), event.getSource().getEntity(), event.getAmount());
+			execute(event, event.getSource(), event.getEntity(), event.getSource().getEntity(), event.getAmount());
 		}
 	}
 
-	public static void execute(Entity entity, Entity sourceentity, double amount) {
-		execute(null, entity, sourceentity, amount);
+	public static void execute(DamageSource damagesource, Entity entity, Entity sourceentity, double amount) {
+		execute(null, damagesource, entity, sourceentity, amount);
 	}
 
-	private static void execute(@Nullable Event event, Entity entity, Entity sourceentity, double amount) {
-		if (entity == null || sourceentity == null)
+	private static void execute(@Nullable Event event, DamageSource damagesource, Entity entity, Entity sourceentity, double amount) {
+		if (damagesource == null || entity == null || sourceentity == null)
 			return;
 		if (EnchantmentHelper.getItemEnchantmentLevel(GrimmsModEnchantments.LIFESTEAL.get(), (sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)) != 0) {
 			if (sourceentity instanceof LivingEntity _entity)
@@ -42,6 +46,11 @@ public class OnEntityAttackedProcedure {
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
 				_entity.addEffect(new MobEffectInstance(GrimmsModMobEffects.BLEEDING, (int) (60 * (sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getEnchantmentLevel(GrimmsModEnchantments.SHARP.get())),
 						(sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getEnchantmentLevel(GrimmsModEnchantments.SHARP.get()), true, false));
+		} else if (damagesource.is(TagKey.create(Registries.DAMAGE_TYPE, new ResourceLocation("grimms:sharpdamage")))) {
+			if (Math.random() < 0.2) {
+				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+					_entity.addEffect(new MobEffectInstance(GrimmsModMobEffects.BLEEDING, 120, 1, true, false));
+			}
 		}
 		ItemXpHandleProcedure.execute(sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY, amount);
 		ItemXpHandleProcedure.execute(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY, amount);
