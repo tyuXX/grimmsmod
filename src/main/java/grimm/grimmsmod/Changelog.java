e7b3033a571d0986c7b1ed2bc0107068a8685b00
tyuXX <tyuxx@gsdistance.org>
2024-09-03 17:16:04 +0300

    update changelog

--- src/main/java/grimm/grimmsmod/Changelog.java
+++ src/main/java/grimm/grimmsmod/Changelog.java
@@ -7,6 +7,5 @@
  * - Fix soulbound and keepinventory
  * - Remove needing to right click the death package for your items
  * - Migrate custom cache loads to a multiple file based system instead of the config based one
- * 
  * - Add grave mechanic
  */
