cef2ad665ba2959a0fdf24e3540ae9ed638feb4f
tyuXX <tyuxx@gsdistance.org>
2024-09-03 18:22:44 +0300

    Push

---
+++ src/main/java/grimm/grimmsmod/world/dimension/VoidDimensionDimension.java
@@ -0,0 +1,32 @@
+
+package grimm.grimmsmod.world.dimension;
+
+import net.neoforged.neoforge.client.event.RegisterDimensionSpecialEffectsEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.api.distmarker.Dist;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+
+public class VoidDimensionDimension {
+	@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+	public static class VoidDimensionSpecialEffectsHandler {
+		@SubscribeEvent
+		public static void registerDimensionSpecialEffects(RegisterDimensionSpecialEffectsEvent event) {
+			DimensionSpecialEffects customEffect = new DimensionSpecialEffects(Float.NaN, true, DimensionSpecialEffects.SkyType.NONE, false, false) {
+				@Override
+				public Vec3 getBrightnessDependentFogColor(Vec3 color, float sunHeight) {
+					return new Vec3(1, 1, 1);
+				}
+
+				@Override
+				public boolean isFoggyAt(int x, int y) {
+					return false;
+				}
+			};
+			event.register(new ResourceLocation("grimms:void_dimension"), customEffect);
+		}
+	}
+}
