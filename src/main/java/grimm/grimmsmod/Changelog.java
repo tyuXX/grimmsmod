1ccfe4161cdb821aa3c787981e6c46b4dd1e155b
tyuXX <gokalpsen09@gmail.com>
2024-09-07 00:39:28 +0300

    Push

--- src/main/java/grimm/grimmsmod/Changelog.java
+++ src/main/java/grimm/grimmsmod/Changelog.java
@@ -1,7 +1,2 @@
 /*
- * - Add a looked block text at HUD
- * - Fixed Test Dummy
- * - Fixed divide by zero error when hitting entity with the sharp enchantment.
- * - Fixed crash when dying in the void.
- * - Fixed bug preventing saving map stats.
  */
